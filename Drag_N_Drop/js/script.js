8991127c21e986564e4efd90cbcb349144567dbf
Tess <tere0506@stud.kea.dk>
2022-03-17 11:52:32 +0100

    done setup

--- Drag_N_Drop/js/script.js
+++ Drag_N_Drop/js/script.js
@@ -1,92 +1,92 @@
 //starting point
-ball.onmousedown = function (event) {
-  // (1) prepare to moving: make absolute and on top by z-index
-  ball.style.position = "absolute";
-  ball.style.zIndex = 1000;
+// person.onmousedown = function (event) {
+//   // (1) prepare to moving: make absolute and on top by z-index
+//   person.style.position = "absolute";
+//   person.style.zIndex = 1000;
 
-  // move it out of any current parents directly into body
-  // to make it positioned relative to the body
-  document.body.append(ball);
+//   // move it out of any current parents directly into body
+//   // to make it positioned relative to the body
+//   document.body.append(person);
 
-  // centers the ball at (pageX, pageY) coordinates
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - ball.offsetWidth / 2 + "px";
-    ball.style.top = pageY - ball.offsetHeight / 2 + "px";
-  }
+//   // centers the person at (pageX, pageY) coordinates
+//   function moveAt(pageX, pageY) {
+//     person.style.left = pageX - person.offsetWidth / 2 + "px";
+//     person.style.top = pageY - person.offsetHeight / 2 + "px";
+//   }
 
-  // move our absolutely positioned ball under the pointer
-  moveAt(event.pageX, event.pageY);
+//   // move our absolutely positioned person under the pointer
+//   moveAt(event.pageX, event.pageY);
 
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
+//   function onMouseMove(event) {
+//     moveAt(event.pageX, event.pageY);
+//   }
 
-  // (2) move the ball on mousemove
-  document.addEventListener("mousemove", onMouseMove);
+//   // (2) move the person on mousemove
+//   document.addEventListener("mousemove", onMouseMove);
 
-  // (3) drop the ball, remove unneeded handlers
-  ball.onmouseup = function () {
-    document.removeEventListener("mousemove", onMouseMove);
-    ball.onmouseup = null;
-  };
-};
+//   // (3) drop the person, remove unneeded handlers
+//   person.onmouseup = function () {
+//     document.removeEventListener("mousemove", onMouseMove);
+//     person.onmouseup = null;
+//   };
+// };
 
 //stopping cloning
-ball.ondragstart = function () {
-  return false;
-};
+// person.ondragstart = function () {
+//   return false;
+// };
 
 //positiononing
-let shiftX = event.clientX - ball.getBoundingClientRect().left;
-let shiftY = event.clientY - ball.getBoundingClientRect().top;
+// let shiftX = event.clientX - person.getBoundingClientRect().left;
+// let shiftY = event.clientY - person.getBoundingClientRect().top;
 
-ball.style.left = event.pageX - shiftX + "px";
-ball.style.top = event.pageY - shiftY + "px";
+// person.style.left = event.pageX - shiftX + "px";
+// person.style.top = event.pageY - shiftY + "px";
 
 //with better posistioning
-ball.onmousedown = function (event) {
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
+// person.onmousedown = function (event) {
+//   let shiftX = event.clientX - person.getBoundingClientRect().left;
+//   let shiftY = event.clientY - person.getBoundingClientRect().top;
 
-  ball.style.position = "absolute";
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
+//   person.style.position = "absolute";
+//   person.style.zIndex = 1000;
+//   document.body.append(person);
 
-  moveAt(event.pageX, event.pageY);
+//   moveAt(event.pageX, event.pageY);
 
-  // moves the ball at (pageX, pageY) coordinates
-  // taking initial shifts into account
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - shiftX + "px";
-    ball.style.top = pageY - shiftY + "px";
-  }
+//   // moves the person at (pageX, pageY) coordinates
+//   // taking initial shifts into account
+//   function moveAt(pageX, pageY) {
+//     person.style.left = pageX - shiftX + "px";
+//     person.style.top = pageY - shiftY + "px";
+//   }
 
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
+//   function onMouseMove(event) {
+//     moveAt(event.pageX, event.pageY);
+//   }
 
-  // move the ball on mousemove
-  document.addEventListener("mousemove", onMouseMove);
+//   // move the person on mousemove
+//   document.addEventListener("mousemove", onMouseMove);
 
-  // drop the ball, remove unneeded handlers
-  ball.onmouseup = function () {
-    document.removeEventListener("mousemove", onMouseMove);
-    ball.onmouseup = null;
-  };
-};
+//   // drop the person, remove unneeded handlers
+//   person.onmouseup = function () {
+//     document.removeEventListener("mousemove", onMouseMove);
+//     person.onmouseup = null;
+//   };
+// };
 
-ball.ondragstart = function () {
-  return false;
-};
+// person.ondragstart = function () {
+//   return false;
+// };
 
 //hiding and droppable
-// in a mouse event handler
-ball.hidden = true; // (*) hide the element that we drag
+// // in a mouse event handler
+// person.hidden = true; // (*) hide the element that we drag
 
-let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-// elemBelow is the element below the ball, may be droppable
+// let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+// // elemBelow is the element below the person, may be droppable
 
-ball.hidden = false;
+// person.hidden = false;
 
 //Final edition
 let currentDroppable = null;
